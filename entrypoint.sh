12e45342008769fd7890a2cba892d70b18025295
tzijnge <someone@somewhere.com>
2025-11-15 15:24:10 +0100

    Update action for towncrier

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,39 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+set -eu # Increase bash strictness
 
-if [ -n "${GITHUB_WORKSPACE}" ] ; then
+if [[ -n "${GITHUB_WORKSPACE}" ]]; then
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
-  git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-misspell -locale="${INPUT_LOCALE}" . \
-  | reviewdog -efm="%f:%l:%c: %m" \
-      -name="linter-name (misspell)" \
-      -reporter="${INPUT_REPORTER:-github-pr-check}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-level="${INPUT_FAIL_LEVEL}" \
-      -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
+export REVIEWDOG_VERSION=v0.21.0
+
+echo "[action-towncrier] Installing reviewdog..."
+wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b /tmp "${REVIEWDOG_VERSION}"
+
+if [[ "$(which towncrier)" == "" ]]; then
+  echo "[action-towncrier] Installing towncrier package..."
+  python -m pip install --upgrade towncrier
+fi
+echo "[action-towncrier] towncrier version:"
+towncrier --version
+
+echo "[action-towncrier] Checking for missing news fragments with towncrier and reviewdog..."
+exit_val="0"
+
+towncrier check --compare_with ${INPUT_TOWNCRIER_COMPARE_WITH}
+  /tmp/reviewdog -efm="%f:%l:%c: %m" \
+    -name="${INPUT_TOOL_NAME}" \
+    -reporter="${INPUT_REPORTER}" \
+    -filter-mode="${INPUT_FILTER_MODE}" \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+    -level="${INPUT_LEVEL}" \
+    ${INPUT_REVIEWDOG_FLAGS} || exit_val="$?"
+
+echo "[action-towncrier] Clean up reviewdog..."
+rm /tmp/reviewdog
+
+if [[ "${exit_val}" -ne '0' ]]; then
+  exit 1
+fi
